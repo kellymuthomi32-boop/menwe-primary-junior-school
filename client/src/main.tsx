17eabe53fe45b77d4bf2e05ce1e3d6aab0714059
kellymuthomi32-boop <kellymuthomi32@gmail.com>
2026-08-29 14:03:39 +0300

    feat: load refined homepage visual system

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import App from "./App";
 import { SupabaseAuthProvider } from "./contexts/SupabaseAuthContext";
 import "./index.css";
 import "./mobile-responsive.css";
+import "./homepage-premium.css";
 
 createRoot(document.getElementById("root")!).render(
   <SupabaseAuthProvider>
