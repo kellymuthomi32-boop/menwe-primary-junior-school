da9e61b2e98b63eec0f580addce2e3a603d77bdb
kellymuthomi32-boop <kellymuthomi32@gmail.com>
2026-08-30 14:32:11 +0300

    style: fix header typography scaling and micro-padding for professional UI

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import { SupabaseAuthProvider } from "./contexts/SupabaseAuthContext";
 import "./index.css";
 import "./mobile-responsive.css";
 import "./homepage-premium.css";
+import "./header-micro.css";
 
 createRoot(document.getElementById("root")!).render(
   <SupabaseAuthProvider>
